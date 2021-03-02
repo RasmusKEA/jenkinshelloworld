2e2fdb4210666c2e76e2e2bf496d3e28f6706279
Rasmus Møller <rasm937k@stud.kea.dk>
2021-03-02 14:16:04 +0100

    test

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.sql.Connection;
+
+@Controller
+public class HomeController {
+
+    public String name= "anna";
+
+    @GetMapping("/")
+    public String home(){
+
+        return "index";
+    }
+
+    public void doSomething(){
+            name = "anna changed";
+    }
+
+}
